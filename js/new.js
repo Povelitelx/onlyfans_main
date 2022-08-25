356719b1e8b209eb19da4d2e9ca8d3bff88e9111
Tarmanau <itarmanau@ibagroup.eu>
2022-08-25 18:19:40 +0300

    add modal

--- js/new.js
+++ js/new.js
@@ -1,7 +1,17 @@
 const burgerNav = document.getElementById('burger_nav')
 const burgerIcon = document.getElementById('burger_icon')
-
+const modalTrigger = document.getElementById('modal_trigger')
+const modalContainer = document.getElementById('modalLayout');
+const closeIcon = document.querySelector('.close-icon');
 
 burgerIcon.onclick = function (){
     burgerNav.classList.toggle('burgerNavOn')
 }
+
+modalTrigger.addEventListener('click', function(){
+    modalContainer.classList.add('openLayout')
+})
+
+closeIcon.addEventListener('click', function(){
+    modalContainer.classList.remove('openLayout')
+})
